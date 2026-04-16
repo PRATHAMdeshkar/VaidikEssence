fd16d0c8dbb1b745349c89e71a64ba0982159108
Prathmesh Deshkar <pratham.deshkar@gmail.com>
2026-04-16 14:38:49 +0530

    Stack Navigator demo (#1)

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -10,6 +11,10 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/about" style={{ marginTop: 20 }}>
+        Go to about
+      </Link>
+      
     </View>
   );
 }
